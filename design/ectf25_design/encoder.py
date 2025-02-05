b4a03e7ffbd571c9a8b012057d24faa7be8187e6
Garrick <schinkelg6461@parkwayschools.net>
2025-02-05 13:32:54 -0600

    Update encoder.py
    Name Change

--- design/ectf25_design/encoder.py
+++ design/ectf25_design/encoder.py
@@ -1,13 +1,9 @@
 """
-Author: Ben Janis
+Author: Garrick Schinkel
 Date: 2025
 
-This source file is part of an example system for MITRE's 2025 Embedded System CTF
-(eCTF). This code is being provided only for educational purposes for the 2025 MITRE
-eCTF competition, and may not meet MITRE standards for quality. Use this code at your
-own risk!
 
-Copyright: Copyright (c) 2025 The MITRE Corporation
+
 """
 
 import argparse
