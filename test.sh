695a1cab907fba33110000ab1e66d2dc4db76f8c
bruberu <80226372+bruberu@users.noreply.github.com>
2025-03-20 16:14:46 -0500

    feat: include secrets in test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+python3 -m ectf25_design.gen_secrets --force ./global.secrets 1 2 3 4
 docker run --rm -v ./build_out:/out -v ./decoder:/decoder -v ./global.secrets:/global.secrets -e DECODER_ID=0xdeadbeef build-decoder
 /home/bruberu/prgm/MaximSDK/Tools/OpenOCD/openocd -s scripts/ -f interface/cmsis-dap.cfg -f target/max78000.cfg -c "init; reset halt; max32xxx mass_erase 0;
  program decoder/insecure.bin verify 0x10000000; program decoder/5a.bin verify 0x10002000; program build_out/max78000.bin 0x1000E000 verify reset exit "
