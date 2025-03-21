6044f40da377826244d3464fc483ac2283ab53c0
bruberu <80226372+bruberu@users.noreply.github.com>
2025-03-21 14:50:00 -0500

    feat: testheadless and testupload scripts

---
+++ testheadless.sh
@@ -0,0 +1,1 @@
+python3 -m ectf25.utils.tester -s ./global.secrets --port /dev/ttyACM0 rand -c 0
---
+++ testupload.sh
@@ -0,0 +1,4 @@
+/home/bruberu/prgm/MaximSDK/Tools/OpenOCD/openocd -s scripts/ -f interface/cmsis-dap.cfg -f target/max78000.cfg -c "init; reset halt; max32xxx mass_erase 0;
+ program decoder/insecure.bin verify 0x10000000; program decoder/5a.bin verify 0x10002000; program build_out/max78000.bin 0x1000E000 verify reset exit "
+sleep 0.5
+python3 -m ectf25.utils.tester -s ./global.secrets --port /dev/ttyACM0 rand -c 0
